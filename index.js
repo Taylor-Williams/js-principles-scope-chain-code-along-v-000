ef4e79904048bb5b5b7bf2c390acc00295bf96c7
Taylor Williams <taylor@thewilliams.net>
2019-02-03 08:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ function changeLeastFavoriteCustomer(name){
   leastFavoriteCustomer = name;
 }
 function attemptTwoFavoriteCustomers(){
-
+  let favoriteCustomer = 'tim';
+  let favoriteCustomer = 'will';
 }
